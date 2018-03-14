911f92c85af62e085e1e9194623095a07e77b76d
drodriguez121m <drodriguez1219@yahoo.com>
2018-03-13 19:32:10 -0500

    updating change

--- public/assets/js/slayer.js
+++ public/assets/js/slayer.js
@@ -4,18 +4,13 @@ var fs = require("fs");
 var tempo = 0;
 var duration = 0;
 // const audio = require("/assets/audio");
-
-<<<<<<< HEAD
-
 var data = fs.readFileSync("../audio/01 Wake Me Up.m4a");
-=======
 // Spotify Widget Player
 // <iframe src="https://open.spotify.com/embed/user/1233302581/playlist/0CxcHM5HGzoAIyrbG8jeZM" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media">
 
->>>>>>> c5350ecb6bb39c3731425f1310ecd46d7358c177
 var calcTempo = function (buffer) {
   var audioData = [];
-  // Take the average of the two channels 
+  // Take the average of the two channels
   if (buffer.numberOfChannels == 2) {
     var channel1Data = buffer.getChannelData(0);
     var channel2Data = buffer.getChannelData(1);

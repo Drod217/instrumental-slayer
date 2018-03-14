1f8a91036779c2a48550cdbb11b9099cdb3013f1
wrrrg <bill.gryta@gmail.com>
2018-03-13 19:18:11 -0500

    Character constructor

--- public/assets/js/slayer.js
+++ public/assets/js/slayer.js
@@ -1,21 +1,19 @@
-var AudioContext = require("web-audio-api").AudioContext;
+  var AudioContext = require("web-audio-api").AudioContext;
 var MusicTempo = require("music-tempo");
 var fs = require("fs");
 var tempo = 0;
 var duration = 0;
 // const audio = require("/assets/audio");
 
-<<<<<<< HEAD
-
 var data = fs.readFileSync("../audio/01 Wake Me Up.m4a");
-=======
+
 // Spotify Widget Player
 // <iframe src="https://open.spotify.com/embed/user/1233302581/playlist/0CxcHM5HGzoAIyrbG8jeZM" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media">
 
->>>>>>> c5350ecb6bb39c3731425f1310ecd46d7358c177
+
 var calcTempo = function (buffer) {
   var audioData = [];
-  // Take the average of the two channels 
+  // Take the average of the two channels
   if (buffer.numberOfChannels == 2) {
     var channel1Data = buffer.getChannelData(0);
     var channel2Data = buffer.getChannelData(1);
@@ -28,8 +26,8 @@ var calcTempo = function (buffer) {
   }
   var p = { expiryTime: 30, maxBeatInterval: 1.5 };
   var mt = new MusicTempo(audioData, p);
-  
- 
+
+
   tempo = mt.tempo;
   duration = ((length/2)/22090);
   console.log("Tempo: " + mt.tempo);
@@ -40,14 +38,13 @@ var calcTempo = function (buffer) {
   // console.log(mt.peaks);
 //   console.log(mt.events);
 }
- 
-// var data = fs.readFileSync("../audio/01 Wake Me Up.m4a");
+
+var data = fs.readFileSync("../audio/01 Wake Me Up.m4a");
 // var data = fs.readFileSync("../audio/seinfeld.mp3");
- 
+
 var context = new AudioContext();
 context.decodeAudioData(data, calcTempo);
 
 console.log(tempo);
 console.log(duration);
 // console.log(context);
-

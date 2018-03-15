5085c98241951b19388925c79f4a448aef292e92
drodriguez121m <drodriguez1219@yahoo.com>
2018-03-15 15:48:10 -0500

    Second song function

--- public/assets/js/slayer.js
+++ public/assets/js/slayer.js
@@ -57,7 +57,7 @@ var calcTempo = function (buffer) {
   tempo = mt.tempo;
   duration = ((length/2)/22090);
   console.log("Tempo: " + mt.tempo);
-  console.log(duration);
+  console.log("Duration: " + duration);
   // console.log("Duration in seconds: "+ (length/2)/22090);
 //   console.log(mt.beats);
   // console.log(mt.spectralFlux);
@@ -71,9 +71,50 @@ var calcTempo = function (buffer) {
 var context = new AudioContext();
 context.decodeAudioData(data, calcTempo);
 
-console.log(tempo);
-console.log(duration);
+// console.log(tempo);
+// console.log(duration);
 // console.log(context);
 
 
 // module.exports = slayer;
+
+random2 = Math.floor(Math.random() * soundTag.length);
+song2 = soundTag[random2];
+var data = fs.readFileSync(song2);
+
+// Spotify Widget Player
+// <iframe src="https://open.spotify.com/embed/user/1233302581/playlist/0CxcHM5HGzoAIyrbG8jeZM" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media">
+
+var calcTempo2 = function (buffer) {
+  var audioData = [];
+  // Take the average of the two channels
+  if (buffer.numberOfChannels == 2) {
+    var channel1Data = buffer.getChannelData(0);
+    var channel2Data = buffer.getChannelData(1);
+    var length = channel1Data.length;
+    for (var i = 0; i < length; i++) {
+      audioData[i] = (channel1Data[i] + channel2Data[i]) / 2;
+    }
+  } else {
+    audioData = buffer.getChannelData(0);
+  }
+  var p = { expiryTime: 30, maxBeatInterval: 1.5 };
+  var mt = new MusicTempo(audioData, p);
+
+
+  tempo = mt.tempo;
+  duration = ((length/2)/22090);
+  console.log("Tempo2: " + mt.tempo);
+  console.log("Duration2: " + duration);
+  // console.log("Duration in seconds: "+ (length/2)/22090);
+//   console.log(mt.beats);
+  // console.log(mt.spectralFlux);
+  // console.log(mt.peaks);
+//   console.log(mt.events);
+}
+
+// var data = fs.readFileSync("../audio/01 Wake Me Up.m4a");
+// var data = fs.readFileSync("../audio/seinfeld.mp3");
+
+var context = new AudioContext();
+context.decodeAudioData(data, calcTempo2);
